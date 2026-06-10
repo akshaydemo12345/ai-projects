ba63d80f3462482934c3d7b218611268ea7f4e35
priti Das <priti@ebrandz.us>
2026-06-10 18:07:54 +0530

    priyanka change

--- frontend/test-api.cjs
+++
@@ -1,3 +0,0 @@
-const grapesjs = require('grapesjs');
-const editor = grapesjs.init({ container: document.createElement('div') });
-console.log(Object.keys(editor.StyleManager.getProperty('typography', 'color')));
